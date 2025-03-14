4a4c68cb8c0d8f4f83004a132f65965f64e399f4
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-03-14 10:54:14 -0300

    criação do servidor com express
    - instalação de pacotes;
    - criação do servidor com express;
    - importação do importação do express para o projeto;
    - configuração do modo "module";
    - configurando a raiz das rotas

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) =>{
+  res.send("Bem Vindo a lojinha!");
+});
+
+app.listen(3000, () => {
+    console.log(" Servidor on, porta 3000");
+});
