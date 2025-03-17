b0e0b9e56159554983f0db13d852e115a3b08894
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-03-16 23:10:03 -0300

    uso do verbo http post
    - uso método de array push()
    - uso do express.json, para receber requisições em json do cliente

--- crud-api/src/index.ts
+++ crud-api/src/index.ts
@@ -2,22 +2,33 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) =>{
   res.send("Bem Vindo a lojinha! - ;-]");
 });
 
+let usuarios = [
+  {
+    nome: "Jéssica Bohn",
+    idade: 28
+  },
+  {
+    nome: "Erich Bönisch",
+    idade: 32
+  }
+];
+
 app.get("/users", (req: Request, res: Response) => {
-  let usuarios = [
-    {
-      nome: "Jéssica Bohn",
-      idade: 28
-    },
-    {
-      nome: "Erich Bönisch",
-      idade: 32
-    }
-  ];
   res.send(usuarios);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  let user = req.body;
+  usuarios.push(user);
+  res.send({
+    message: "Usuário criado com sucesso!"
+  })
 });
 
 app.listen(3000, () => {
