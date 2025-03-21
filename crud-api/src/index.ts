df1720f8bcaaa0ff911b576092d00d40acde6b2f
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-03-21 20:31:17 -0300

    Parametros de rota com PUT
    - implementação do verbo PUT;
    - definição de Type;

--- crud-api/src/index.ts
+++ crud-api/src/index.ts
@@ -10,12 +10,14 @@ app.get("/", (req: Request, res: Response) => {
 
 let id = 0;
 
-let usuarios: {
+type User = {
   id: number,
   nome: string,
   email: string,
   idade: number
-}[] = [];
+};
+
+let usuarios: User[] = [];
 
 app.get("/users", (req: Request, res: Response) => {
   res.send(usuarios);
@@ -34,6 +36,20 @@ app.post("/users", (req: Request, res: Response) => {
   res.send({
     message: "Usuário criado com sucesso!"
   })
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let user = req.body;
+  let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
+
+  usuarios[indexOf].nome = user.nome;
+  usuarios[indexOf].email = user.email;
+  usuarios[indexOf].idade = user.idade;
+
+  res.send({
+    message: "Usuário alterado com sucesso!"
+  });
 });
 
 app.listen(3000, () => {
