5c19ba18267ce2e8323bb4f3505451caaca0d359
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-03-23 23:17:28 -0300

    Organizado Rotas com Router
    - criação de um orquestrador de rotas  de usuários, users.route.ts
    - criação de um novo index.ts que vai orquestrar todas as outras rotas a serem criadas;

--- crud-api/src/index.ts
+++ crud-api/src/index.ts
@@ -1,65 +1,9 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { routes } from "./routes/index";
 
 const app = express();
 
-app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Bem Vindo a lojinha! - ;-]");
-});
-
-let id = 0;
-
-type User = {
-  id: number,
-  nome: string,
-  email: string,
-  idade: number
-};
-
-let usuarios: User[] = [];
-
-app.get("/users", (req: Request, res: Response) => {
-  res.send(usuarios);
-});
-
-app.get("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let user = usuarios.find(user => user.id === userId);
-  res.send(user);
-});
-
-app.post("/users", (req: Request, res: Response) => {
-  let user = req.body;
-  user.id = ++id;
-  usuarios.push(user);
-  res.send({
-    message: "Usuário criado com sucesso!"
-  })
-});
-
-app.put("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let user = req.body;
-  let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
-
-  usuarios[indexOf].nome = user.nome;
-  usuarios[indexOf].email = user.email;
-  usuarios[indexOf].idade = user.idade;
-
-  res.send({
-    message: "Usuário alterado com sucesso!"
-  });
-});
-
-app.delete("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let indexOf = usuarios.findIndex((user: User) => user.id === userId);
-  usuarios.splice(indexOf, 1);
-  res.send({
-    message: "Usuário excluído com sucesso!"
-  })
-});
+routes(app);
 
 app.listen(3000, () => {
   console.log(" Servidor on, porta 3000");
