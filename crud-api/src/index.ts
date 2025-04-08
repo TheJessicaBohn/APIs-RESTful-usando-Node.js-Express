d5f7a67be1228e6225518cb72d4bfd1aafeef610
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-04-07 23:28:39 -0300

    Tratamento de Middleware Page NotFound
    - tratamento para tentativa de acesso de rota e a mesma não existir;
    - método de tratamento que pode ser reutilizado de notfound

--- crud-api/src/index.ts
+++ crud-api/src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { initializeApp } from 'firebase-admin/app';
 import { routes } from "./routes/index";
 import { errorHandler } from "./middlewares/error-handler.middleware";
+import { pageNoteFoundHadler } from "./middlewares/page-not-found.middleare";
 
 initializeApp();
 const app = express();
 
 routes(app);
 errorHandler(app);
+pageNoteFoundHadler(app);
 
 app.listen(3000, () => {
   console.log(" Servidor on, porta 3000");
