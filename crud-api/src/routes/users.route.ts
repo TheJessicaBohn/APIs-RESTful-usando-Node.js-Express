0a0b0f42b354f739cf4b87bf497bb68cb67e51b2
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-04-10 22:58:18 -0300

    Validação de dados com Celebrate
    - criação de validações por meio do Joi:
    - utilização de required;
    - bloqueio de conteúdo inválido;
    - utiliza o middleware de erro do próprio Celebrate;

--- crud-api/src/routes/users.route.ts
+++ crud-api/src/routes/users.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { UsersController } from "../controllers/users.controllers";
 import expressAsyncHandler from "express-async-handler";
+import { celebrate, Joi, Segments } from "celebrate";
 
 export const userRoutes = express.Router();
 
@@ -8,7 +9,13 @@ userRoutes.get("/users", expressAsyncHandler(UsersController.getAll));
 
 userRoutes.get("/users/:id", expressAsyncHandler(UsersController.getById));
 
-userRoutes.post("/users", expressAsyncHandler(UsersController.create));
+userRoutes.post("/users", celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().email().required(),
+    idade: Joi.number().required(),
+    nome: Joi.string().required()
+  })
+}), expressAsyncHandler(UsersController.create));
 
 userRoutes.put("/users/:id", expressAsyncHandler(UsersController.update));
 
